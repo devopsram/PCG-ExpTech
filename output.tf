20b5828264226df3763983a2dc4345febad9508e
devopsram <31151487+devopsram@users.noreply.github.com>
2025-03-07 11:02:22 +0530

    Update output.tf

---
+++ output.tf
@@ -0,0 +1,8 @@
+# Output ECS Cluster Information
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.ecs_cluster.name
+}
+
+output "ecs_service_role_arn" {
+  value = aws_iam_role.ecs_service_role.arn
+}
