554d1827e557a423618297ffbf75fcb1fd4c86a7
ram <ramveer.vm@gmail.com>
2025-03-08 12:33:01 +0530

    update

--- main.tf
+++ main.tf
@@ -348,10 +348,6 @@ resource "aws_ecs_service" "ecs_service" {
    type = "distinctInstance"
  }
 
- triggers = {
-   redeployment = timestamp()
- }
-
  capacity_provider_strategy {
    capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
    weight            = 100
@@ -361,6 +357,10 @@ resource "aws_ecs_service" "ecs_service" {
    target_group_arn = aws_lb_target_group.ecs_tg.arn
    container_name   = "web"
    container_port   = 80
+ }
+
+ triggers = {
+   redeployment = timestamp()
  }
 
  depends_on = [aws_autoscaling_group.ecs_asg]
