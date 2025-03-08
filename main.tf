e436bbe03fd20f85958db53053b40737e4426334
ram <ramveer.vm@gmail.com>
2025-03-08 11:12:49 +0530

    task role and execution role added

--- main.tf
+++ main.tf
@@ -190,8 +190,46 @@ resource "aws_iam_role" "ecs_service_role" {
 }
 
 # Attach Policy to ECS Service Role
-resource "aws_iam_role_policy_attachment" "ecs_service_policy" {
-  role       = aws_iam_role.ecs_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSServiceRolePolicy"
+# resource "aws_iam_role_policy_attachment" "ecs_service_policy" {
+#   role       = aws_iam_role.ecs_service_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonECSServiceRolePolicy"
+# }
+
+
+# Create ECS Execution Role
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      }
+    ]
+  })
 }
 
+# Create ECS Task Role
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecsTaskRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      }
+    ]
+  })
+}
