b5eb038acf84a512c38158b054f952effc1a6b2f
ram <ramveer.vm@gmail.com>
2025-03-08 12:19:11 +0530

    loadbalancer, capacity provider, ecs service added

--- main.tf
+++ main.tf
@@ -160,8 +160,8 @@ resource "aws_launch_template" "ecs_launch_template" {
 
 # Auto Scaling Group for ECS Instances
 resource "aws_autoscaling_group" "ecs_asg" {
-  desired_capacity = 1
-  max_size         = 2
+  desired_capacity = 2
+  max_size         = 3
   min_size         = 1
 
   launch_template {
@@ -170,7 +170,77 @@ resource "aws_autoscaling_group" "ecs_asg" {
   }
 
   vpc_zone_identifier = [aws_subnet.subnets[0].id]
+  tag {
+   key                 = "AmazonECSManaged"
+   value               = true
+   propagate_at_launch = true
  
+}
+}
+
+# Application Load balancer with Target group 
+resource "aws_lb" "ecs_alb" {
+ name               = "ecs-alb"
+ internal           = false
+ load_balancer_type = "application"
+ security_groups    = [aws_security_group.app-sg.id]
+ subnets            = [aws_subnet.subnets[0].id]
+
+ tags = {
+   Name = "ecs-alb"
+ }
+}
+
+resource "aws_lb_listener" "ecs_alb_listener" {
+ load_balancer_arn = aws_lb.ecs_alb.arn
+ port              = 80
+ protocol          = "HTTP"
+
+ default_action {
+   type             = "forward"
+   target_group_arn = aws_lb_target_group.ecs_tg.arn
+ }
+}
+
+resource "aws_lb_target_group" "ecs_tg" {
+ name        = "ecs-target-group"
+ port        = 80
+ protocol    = "HTTP"
+ target_type = "ip"
+ vpc_id      = aws_vpc.primary_vpc.id
+
+ health_check {
+   path = "/"
+ }
+}
+
+#Capacity Providers
+
+resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
+ name = "test1"
+
+ auto_scaling_group_provider {
+   auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
+
+   managed_scaling {
+     maximum_scaling_step_size = 1000
+     minimum_scaling_step_size = 1
+     status                    = "ENABLED"
+     target_capacity           = 3
+   }
+ }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "ecs_cluster_capacity_provider" {
+ cluster_name = aws_ecs_cluster.ecs_cluster.name
+
+ capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
+
+ default_capacity_provider_strategy {
+   base              = 1
+   weight            = 100
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+ }
 }
 
 # ECS Service Role
@@ -251,7 +321,7 @@ resource "aws_ecs_task_definition" "task" {
       essential = true
       portMappings = [
         {
-          containerPort = 80
+          containerPort = 5000
           hostPort      = 80
           protocol      = "tcp"
         }
@@ -259,3 +329,40 @@ resource "aws_ecs_task_definition" "task" {
     }
   ])
 }
+
+## ECS service
+
+resource "aws_ecs_service" "ecs_service" {
+ name            = "my-ecs-service"
+ cluster         = aws_ecs_cluster.ecs_cluster.id
+ task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+ desired_count   = 2
+
+ network_configuration {
+   subnets         = [aws_subnet.subnets[0].id]
+   security_groups = [aws_security_group.app-sg.id]
+ }
+
+ force_new_deployment = true
+ placement_constraints {
+   type = "distinctInstance"
+ }
+
+ triggers = {
+   redeployment = timestamp()
+ }
+
+ capacity_provider_strategy {
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+   weight            = 100
+ }
+
+ load_balancer {
+   target_group_arn = aws_lb_target_group.ecs_tg.arn
+   container_name   = "web"
+   container_port   = 80
+ }
+
+ depends_on = [aws_autoscaling_group.ecs_asg]
+}
+
