85c5ac32b8ca3ea7f47b2911878e18eec074759b
ram <ramveer.vm@gmail.com>
2025-03-09 14:22:42 +0530

    ecs.sh file updated

--- ecs.sh
+++ ecs.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+#echo ECS_CLUSTER=pcg-ecs-cluster >> /etc/ecs/ecs.config
+#!/bin/bash
+
+# Update all packages
+
+sudo yum update -y
+sudo yum install -y ecs-init
+sudo service docker start
+sudo start ecs
+
+#Adding cluster name in ecs config
 echo ECS_CLUSTER=pcg-ecs-cluster >> /etc/ecs/ecs.config
+cat /etc/ecs/ecs.config | grep "ECS_CLUSTER"
