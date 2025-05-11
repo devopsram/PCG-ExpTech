d717eb5f443526d5e8ffda3b818502335f1ef9f7
ram <ramveer.vm@gmail.com>
2025-05-11 11:21:30 +0530

    folder updated

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     # Replace this with your bucket name!
     bucket         = "pcg-tf-state"
-    key            = "pcg/terraform.tfstate"
+    key            = "pcg1/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
