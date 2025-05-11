18a32a10aff9929c92bc692c0afb68ea3ff62a09
ram <ramveer.vm@gmail.com>
2025-05-11 11:07:00 +0530

    InfraCreationRole added as assumrRole

--- main.tf
+++ main.tf
@@ -233,7 +233,7 @@ resource "aws_iam_instance_profile" "ecs_instance_profile" {
 # Launch Configuration for ECS Instances
 resource "aws_launch_template" "ecs_launch_template" {
   name_prefix   = "ecs-launch-template-"
-  image_id      = "ami-0a2beeba6d1093556" # Replace with a valid ECS-optimized AMI ID
+  image_id      = "ami-0654044a68c6cdd61" # Replace with a valid ECS-optimized AMI ID 
   instance_type = "t3.medium"              # Adjust instance type as needed
   key_name = "ecsInstance"
   
--- provider.tf
+++ provider.tf
@@ -9,4 +9,7 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
+  assume_role {
+    role_arn = "arn:aws:iam::970547357901:role/InfraCreationRole" # Replace with your role ARN
+  }
 }
